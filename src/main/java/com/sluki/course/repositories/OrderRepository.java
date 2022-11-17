46f668b45026bca00b3147c9581e71db5bb7b707
bruno santos <bruno4t@hotmail.com>
2022-11-17 20:54:07 -0300

    criação da classe order e relacionamento com classe user

---
+++ src/main/java/com/sluki/course/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.sluki.course.repositories;
+
+import com.sluki.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+
+}
